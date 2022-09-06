5d77c6e958b9e6e392f1b6eed6ff35e3fd3d8196
simon <simonoche987@gmail.com>
2022-09-06 23:58:24 +0100

    useReducer to manage state

--- commercefrontend/src/pages/HomePage.js
+++ commercefrontend/src/pages/HomePage.js
@@ -1,16 +1,50 @@
-import React, { useEffect, useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useReducer, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import logger from 'use-reducer-logger'
 //import data from '../data'
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload};
+    default:
+      return state;
+  }
+};
+//reducer accepts two parameters: the current state, ...
+//...and the action that changes and creates a new state. We define a switchcase inside the useReducer
 
 //I use Link instead of Anchor in order to fix the page refresh, as I want to creat a single page application with multiple screens
 const HomePage = () => {
+ 
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  })
+  //Define an array that contains two parameters: an object and "dispatch" to call an action and update state
+  //useReducer accepts two parameters: the reducer and the default state
 
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+
+  //dispatch the FETCH_REQUEST case before the ajax code so the loader can show while waiting for data from the api
   useEffect(()=>{
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-    setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch(err) {
+        dispatch({type: 'FETCH_FAIL', payload: err.message})
+      }
+      
+      //setProducts(result.data);
     };
     fetchData();
   },)
@@ -25,6 +59,11 @@ const HomePage = () => {
         <h1>Featured Products</h1>
         <div className="products">
         {
+          loading ? ( 
+          <div>Loading...</div>
+        ) : error ? ( 
+          <div>{error}</div>
+        )  : (
           products.map(product => 
           (<div className="productItem" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
@@ -42,10 +81,13 @@ const HomePage = () => {
             <button>Add to cart</button>
             </div>
           </div>))
-        }
+        )}
         </div>
     </>
   )
 }
+
+
+//Install package to log state changes of dispatching actions "npm install use-reducer-logger --force"
 
 export default HomePage
